f703026450741bd1d2f1b2b3c2c10f4250800d1b
Celery Qin <qinhuijie@qq.com>
2017-08-06 00:15:10 +0800

    paramezation,start hyperplane

--- linsys.py
+++ linsys.py
@@ -151,9 +151,188 @@ class LinearSystem(object):
         -Each pivot variable has coefficient 1
         -Each pivot variable is in own column
         -(Technical)Any 0=k equations should be 0=1
-		 should come before 0=0
+                 should come before 0=0
         """
-        return deepcopy(self)
+        tf = self.compute_triangular_form()
+
+        num_equations = len(tf)
+        pivot_indices = tf.indices_of_first_nonzero_terms_in_each_row()
+
+        for i in range(num_equations)[::-1]:
+            j = pivot_indices[i]
+            if j < 0:  # 如果系数全部为0
+                continue
+            tf.scale_row_to_make_coefficient_equal_one(i, j)
+            tf.clear_coefficient_above(i, j)
+
+        return tf
+
+    def scale_row_to_make_coefficient_equal_one(self, row, col):
+        n = self[row].normal_vector
+        beta = Decimal('1.0') / n[col]
+        self.multiply_coefficient_and_row(beta, row)
+
+    def clear_coefficient_above(self, row, col):
+        for k in range(row)[::-1]:
+            n = self[k].normal_vector
+            alpha = -(n[col])
+            self.add_multiple_times_row_to_row(alpha, row, k)
+
+    def compute_extract_solution(self):
+        try:
+            return self.do_gaussian_elimination_and_extract_solution()
+        except Exception as e:
+            if (str(e) == self.NO_SOLUTIONS_MSG or
+                    str(e) == self.INF_SOLUTIONS_MSG):
+                return str(e)
+            else:
+                raise e
+
+    def compute_parametrize_solution(self):
+        try:
+            return self.do_gaussian_elimination_and_parametrize_solution()
+        except Exception as e:
+            if str(e) == self.NO_SOLUTIONS_MSG:
+                return str(e)
+            else:
+                raise e
+
+    def do_gaussian_elimination_and_parametrize_solution(self):
+        rref = self.compute_rref()
+
+        rref.raise_exception_if_contradictory_equation()
+
+        direction_vectors = rref.extract_direction_vectors_for_parametrization()
+        basepoint = rref.extract_basepoint_for_parametrizetion()
+
+        return Parametrization(basepoint, direction_vectors)
+
+    def extract_direction_vectors_for_parametrization(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        free_variable_indices = set(range(num_variables)) - set(pivot_indices)
+
+        direction_vectors = []
+
+        for free_var in free_variable_indices:  # 如果以free_var为主变量
+            vector_coords = [0] * num_variables
+            vector_coords[free_var] = 1
+            for i, p in enumerate(self.planes):
+                pivot_var = pivot_indices[i]
+                if pivot_var < 0:  # 0=k
+                    break
+                vector_coords[pivot_var] = -p.normal_vector[free_var]
+            direction_vectors.append(Vector(vector_coords))
+
+        return direction_vectors
+
+    def extract_basepoint_for_parametrizetion(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+
+        basepoint_coords = [0] * num_variables
+
+        for i, p in enumerate(self.planes):
+            pivot_var = pivot_indices[i]
+            if pivot_var < 0:
+                break
+            basepoint_coords[pivot_var] = p.constant_term
+
+        return Vector(basepoint_coords)
+
+    def do_gaussian_elimination_and_extract_solution(self):
+        rref = self.compute_rref()
+
+        rref.raise_exception_if_contradictory_equation()
+        rref.raise_exception_if_too_few_pivots()
+
+        num_variables = rref.dimension
+        solution_coordinates = [rref.planes[
+            i].constant_term for i in range(num_variables)]
+        return Vector(solution_coordinates)
+
+    def raise_exception_if_contradictory_equation(self):
+        """0=k,矛盾的条件"""
+        for p in self.planes:
+            try:
+                p.first_nonzero_index(p.normal_vector)
+            except Exception as e:
+                if str(e) == 'No nonzero elements found':
+                    constant_term = MyDecimal(p.constant_term)
+                    if not constant_term.is_near_zero():
+                        raise Exception(self.NO_SOLUTIONS_MSG)
+                else:
+                    raise e
+
+    def raise_exception_if_too_few_pivots(self):
+        '''多个解的情况'''
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        num_pivots = sum([1 if index >= 0 else 0 for index in pivot_indices])
+        num_variables = self.dimension
+
+        if num_pivots < num_variables:
+            raise Exception(self.INF_SOLUTIONS_MSG)
+
+
+class Parametrization(object):
+    """编写参数化函数"""
+    BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG = (
+        'The basepoint and direction vectors should all live in the same dimension')
+
+    def __init__(self, basepoint, direction_vectors):
+        self.basepoint = basepoint
+        self.direction_vectors = direction_vectors
+        self.dimension = self.basepoint.dimension
+
+        try:
+            for v in direction_vectors:
+                assert v.dimension == self.dimension
+        except AssertionError:
+            raise Exception(BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
+
+    def __str__(self):
+        num_decimal_places = 3
+
+        def write_coefficient(coefficient, is_initial_term=False):
+            coefficient = round(coefficient, num_decimal_places)
+            if coefficient % 1 == 0:
+                coefficient = int(coefficient)
+
+            output = ''
+
+            if coefficient < 0:
+                output += '-'
+            if coefficient > 0 and not is_initial_term:
+                output += '+'
+
+            if not is_initial_term:
+                output += ' '
+
+            if abs(coefficient) != 1:
+                output += '{}'.format(abs(coefficient))
+
+            return output
+
+        n = self.normal_vector
+
+        try:
+            initial_index = Plane.first_nonzero_index(n)
+            terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
+                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+            output = ' '.join(terms)
+
+        except Exception as e:
+            if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
+                output = '0'
+            else:
+                raise e
+
+        constant = round(self.constant_term, num_decimal_places)
+        if constant % 1 == 0:
+            constant = int(constant)
+        output += ' = {}'.format(constant)
+
+        return output
 
 
 class MyDecimal(Decimal):
@@ -264,41 +443,83 @@ class MyDecimal(Decimal):
 #     print 'test case 4 failed'
 
 # 编写REFF函数
-p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='2')
-s = LinearSystem([p1, p2])
-r = s.compute_rref()
-# pdb.set_trace()
-if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='-1') and
-        r[1] == p2):
-    print 'test case 1 failed'
+# p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+# p2 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='2')
+# s = LinearSystem([p1, p2])
+# r = s.compute_rref()
+# # pdb.set_trace()
+# if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='-1') and
+#         r[1] == p2):
+#     print 'test case 1 failed'
 
-p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='2')
-s = LinearSystem([p1, p2])
-r = s.compute_rref()
-if not (r[0] == p1 and
-        r[1] == Plane(constant_term='1')):
-    print 'test case 2 failed'
+# p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+# p2 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='2')
+# s = LinearSystem([p1, p2])
+# r = s.compute_rref()
+# if not (r[0] == p1 and
+#         r[1] == Plane(constant_term='1')):
+#     print 'test case 2 failed'
 
-p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0', '1', '0']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1', '1', '-1']), constant_term='3')
-p4 = Plane(normal_vector=Vector(['1', '0', '-2']), constant_term='2')
-s = LinearSystem([p1, p2, p3, p4])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='0') and
-        r[1] == p2 and
-        r[2] == Plane(normal_vector=Vector(['0', '0', '-2']), constant_term='2') and
-        r[3] == Plane()):
-    print 'test case 3 failed'
+# p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+# p2 = Plane(normal_vector=Vector(['0', '1', '0']), constant_term='2')
+# p3 = Plane(normal_vector=Vector(['1', '1', '-1']), constant_term='3')
+# p4 = Plane(normal_vector=Vector(['1', '0', '-2']), constant_term='2')
+# s = LinearSystem([p1, p2, p3, p4])
+# r = s.compute_rref()
+# if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='0') and
+#         r[1] == p2 and
+#         r[2] == Plane(normal_vector=Vector(['0', '0', '-2']), constant_term='2') and
+#         r[3] == Plane()):
+#     print 'test case 3 failed'
 
-p1 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1', '-1', '1']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1', '2', '-5']), constant_term='3')
-s = LinearSystem([p1, p2, p3])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term=Decimal('23') / Decimal('9')) and
-        r[1] == Plane(normal_vector=Vector(['0', '1', '0']), constant_term=Decimal('7') / Decimal('9')) and
-        r[2] == Plane(normal_vector=Vector(['0', '0', '1']), constant_term=Decimal('2') / Decimal('9'))):
-    print 'test case 4 failed'
+# p1 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1')
+# p2 = Plane(normal_vector=Vector(['1', '-1', '1']), constant_term='2')
+# p3 = Plane(normal_vector=Vector(['1', '2', '-5']), constant_term='3')
+# s = LinearSystem([p1, p2, p3])
+# r = s.compute_rref()
+# if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term=Decimal('23') / Decimal('9')) and
+#         r[1] == Plane(normal_vector=Vector(['0', '1', '0']), constant_term=Decimal('7') / Decimal('9')) and
+#         r[2] == Plane(normal_vector=Vector(['0', '0', '1']), constant_term=Decimal('2') / Decimal('9'))):
+#     print 'test case 4 failed'
+
+
+# 编写高斯消去法求解函数
+# p1 = Plane(normal_vector=Vector([5.862, 1.178, -10.366]), constant_term=-8.15)
+# p2 = Plane(normal_vector=Vector([-2.931, -0.589, 5.183]), constant_term=-4.075)
+# s = LinearSystem([p1, p2])
+# t = s.compute_solution()
+# print t
+
+# p1 = Plane(normal_vector=Vector([8.631, 5.112, -1.816]), constant_term=-5.113)
+# p2 = Plane(normal_vector=Vector([4.315, 11.132, -5.27]), constant_term=-6.775)
+# p3 = Plane(normal_vector=Vector([-2.158, 3.01, -1.727]), constant_term=-0.831)
+# s = LinearSystem([p1, p2, p3])
+# t = s.compute_solution()
+# print t
+
+# p1 = Plane(normal_vector=Vector([5.262, 2.739, -9.878]), constant_term=-3.441)
+# p2 = Plane(normal_vector=Vector([5.111, 6.358, 7.638]), constant_term=-2.152)
+# p3 = Plane(normal_vector=Vector([2.016, -9.924, -1.367]), constant_term=-9.278)
+# p4 = Plane(normal_vector=Vector(
+#     [2.167, -13.543, -18.883]), constant_term=-10.567)
+# s = LinearSystem([p1, p2, p3, p4])
+# t = s.compute_solution()
+# print t
+
+# 编写参数化函数
+p0 = Plane(normal_vector=Vector([0.786, 0.786, 0.588]), constant_term=-0.714)
+p1 = Plane(normal_vector=Vector([-0.138, -0.138, 0.244]), constant_term=0.319)
+s = LinearSystem([p0, p1])
+print 'System 1 solution:\n{}'.format(s.compute_parametrize_solution())
+
+p0 = Plane(normal_vector=Vector([8.631, 5.112, -1.816]), constant_term=-5.113)
+p1 = Plane(normal_vector=Vector([4.315, 11.132, -5.27]), constant_term=-6.775)
+p2 = Plane(normal_vector=Vector([-2.158, 3.01, -1.727]), constant_term=-0.831)
+
+p0 = Plane(normal_vector=Vector([0.935, 1.76, -9.365]), constant_term=-9.955)
+p1 = Plane(normal_vector=Vector([0.187, 0.352, -1.873]), constant_term=-1.991)
+p2 = Plane(normal_vector=Vector([0.374, 0.704, -3.746]), constant_term=-3.982)
+p3 = Plane(normal_vector=Vector([-0.561, -1.056, 5.619]), constant_term=5.973)
+
+
+s = LinearSystem([p0, p1, p2, p3])
